6c2346ad7569121bec5ba2d34c2c68165497e3dc
Divyesh Vishwakarma <divyesh1099@gmail.com>
2024-02-07 03:28:52 +0530

    Allow All Hosts

--- defenseFormatterWebApp/defenseFormatterWebApp/settings.py
+++ defenseFormatterWebApp/defenseFormatterWebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fog^@b-p+hsqa7qf%$#@&08mbksav=fgr+&)aevif3&6!cj8!w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
